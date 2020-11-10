b0c0589de86b70d686de66dc812352b6ff4cc8a6
Resolas <64108044+Resolas@users.noreply.github.com>
2020-11-10 17:53:57 +0000

    Folders and Package Setup
    Folders for organization obviously
    
    Packages for use in scene modelling and effects

---
+++ Game Engines CA1 Alien Invasion Game/Assets/_Scripts/CityPrefabTable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/CityPrefabTable", order = 1)]
+public class CityPrefabTable : ScriptableObject
+{
+
+    public GameObject[] roofModules;
+    public GameObject[] bodyModules;
+
+
+}
