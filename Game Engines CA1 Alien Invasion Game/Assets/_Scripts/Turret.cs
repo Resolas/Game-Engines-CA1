3ff14e85c0a1fc787e2f8f6436e0384db8621cc0
Resolas <64108044+Resolas@users.noreply.github.com>
2020-12-02 04:41:51 +0000

    Turret Targeting Functionality
    - A universal turret targeting script works for both enemies and friendlies
    - Tick bools in the inspector to include/exclude tagged targets in range

--- Game Engines CA1 Alien Invasion Game/Assets/_Scripts/Turret.cs
+++ Game Engines CA1 Alien Invasion Game/Assets/_Scripts/Turret.cs
@@ -19,14 +19,14 @@ public class Turret : MonoBehaviour
     public float range = 250f;
     private float closest;
     public bool isFriendly = false;
-    public float getDist;
+    public float getDistToTarget;
 
 
     // Update is called once per frame
     void FixedUpdate()
     {
 
-     //   TrackTarget();
+        Debug.DrawLine(myTarget.transform.position, transform.position, Color.red);
 
     }
 
@@ -39,39 +39,59 @@ public class Turret : MonoBehaviour
 
     public float trackRange = 100f;
 
-    private Transform closestTarget;
-
+    
+    
 
     void TrackTarget()
     {
 
         Collider[] targets = Physics.OverlapSphere(transform.position,trackRange);
 
+        float nearestDist = Mathf.Infinity;
+        Transform closestTarget = null;
+
         foreach (Collider target in targets)
         {
-            if (target.CompareTag("Untagged")) continue;    // Ignore everything that's Untagged
+            if (target.CompareTag("Untagged") || target.CompareTag("Projectile")) continue;    // Ignore everything that's Untagged
             if (target.CompareTag("Enemy") && findEnemy != true) continue;
             if (target.CompareTag("Friendly") && findFriendly != true) continue;
             if (target.CompareTag("Player") && findPlayer != true) continue;
             if (target.CompareTag("Structure") && findStructure != true) continue;
 
 
-            Vector3 pointAB = target.transform.position - transform.position;
-                getDist = pointAB.sqrMagnitude;
 
-                Debug.Log("TrackTest");
-                Debug.DrawLine(target.transform.position, transform.position);
-
-
-
-                myTarget = closestTarget;   // Finalized Target
-
+            //    Vector3 pointAB = target.transform.position - transform.position;
+            getDistToTarget = Vector3.Distance(transform.position,target.transform.position);
             
-            
+            Debug.DrawLine(target.transform.position, transform.position, Color.white);
+            if (getDistToTarget <= nearestDist)
+            {
+                nearestDist = getDistToTarget;
+                closestTarget = target.GetComponent<Transform>();
                 
-            
+            }
+
+            if (closestTarget != null && nearestDist < trackRange)
+            {
+                myTarget = closestTarget.transform;
+                
+                Debug.Log("TrackTestHasTarget");
+            }
+            else
+            {
+                myTarget = null;
+                Debug.Log("TrackTestNull");
+            }
+            Debug.Log("nearest target " + nearestDist + " " + closestTarget + " " + myTarget);
 
             
+
+
+
+
+         //   myTarget = closestTarget.GetComponent<Transform>();   // Finalized Target
+
+ 
 
         }
 
@@ -104,7 +124,7 @@ public class Turret : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.cyan;
-        Gizmos.DrawWireSphere(transform.position,range);
+        Gizmos.DrawWireSphere(transform.position,trackRange);
 
     }
 
